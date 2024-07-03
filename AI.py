e845485dfafd28a9215681e7d7285372e9686f10
Вадим Волков <gigafulldom@gmail.com>
2024-07-03 14:37:32 +0300

    update: change layers in AI

--- AI.py
+++ AI.py
@@ -11,7 +11,7 @@ plt.style.use('fivethirtyeight')
 file_name_for_AAPL = 'data/AAPL_stock_data.csv'
 file_name_for_MSFT = 'data/MSFT_stock_data.csv'
 file_name_for_NVDA = 'data/NVDA_stock_data.csv'
-df = pd.read_csv(file_name_for_NVDA)
+df = pd.read_csv(file_name_for_MSFT)
 data = df.filter(['Close'])
 dataset = data.values
 traning_data_len = math.ceil(len(dataset)*.8)
@@ -26,16 +26,16 @@ for i in range(60,len(traning_data)):
 x_train,y_train =np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train,(x_train.shape[0],x_train.shape[1],1))
 model = Sequential()
-model.add(LSTM(50,return_sequences=True,input_shape=(x_train.shape[1],1)))
-model.add(LSTM(50,return_sequences=False))
+model.add(LSTM(100,return_sequences=True,input_shape=(x_train.shape[1],1)))
+model.add(LSTM(75,return_sequences=False))
 model.add(Dense(25))
 model.add(Dense(1))
 model.compile(optimizer='adam',loss='mean_squared_error')
 model.fit(x_train,y_train,batch_size=1,epochs=1)
-model.save('ai_models/ai_for_NVDA_price_pridiction.h5')
+#model.save('ai_models/ai_for_MSFT_price_pridiction.h5')
 
 
-'''test_data = scaler_data[traning_data_len-60:,:]
+test_data = scaler_data[traning_data_len-60:,:]
 x_test=[]
 y_test = dataset[traning_data_len:,:]
 for i in range(60,len(test_data)):
@@ -43,4 +43,19 @@ for i in range(60,len(test_data)):
 x_test=np.array(x_test)
 x_test = np.reshape(x_test,(x_test.shape[0],x_test.shape[1],1))
 predictions = model.predict(x_test)
-predictions = scaler.inverse_transform(predictions)'''
+predictions = scaler.inverse_transform(predictions)
+# Построение графика
+train = data[:traning_data_len]
+valid = data[traning_data_len:]
+valid['Predictions'] = predictions
+
+plt.figure(figsize=(16,8))
+plt.title('Model')
+plt.xlabel('Date', fontsize=18)
+plt.ylabel('Close Price USD ($)', fontsize=18)
+plt.plot(train['Close'])
+plt.plot(valid[['Close', 'Predictions']])
+plt.legend(['Train', 'Actual', 'Predictions'], loc='lower right')
+plt.show()
+rmse = np.sqrt(np.mean(predictions - y_test )**2)
+print(rmse)
